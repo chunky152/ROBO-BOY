10ea06fc775cdf6b9f219da7482c1f9f5ff06793
chunky152 <alvinmuwanguzi1@gmail.com>
2025-06-16 16:39:03 +0300

    added a menu icon to the activity navigation window

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.annotation)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
